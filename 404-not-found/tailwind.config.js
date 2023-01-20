3a40b60456529df267f14068d5c6af636f8d9539
linelinlove <chrisline.lin@outlook.fr>
2023-01-20 21:50:58 +0100

    media responsive

---
+++ 404-not-found/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            s: ['10px', '16px'],
+            sm: ['14px', '20px'],
+            base: ['16px', '24px'],
+            lg: ['20px', '28px'],
+
+            xl: ['48px', '48px'],
+
+            head: ['24px', '24px'],
+
+            title: ['64px', '64x'],
+            text: ['18px', '18px'],
+            footer: ['14px', '14px'],
+            button: ['14px', '14px'],
+        },
+        extend: {
+            fontFamily: {
+                'inconsolata': ['"Inconsolata"'],
+                'space-mono': ['"Space Mono"'],
+                'montserrat': ['"Montserrat"']
+            }
+        }
+    }
+}
