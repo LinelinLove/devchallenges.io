467b2f77c87a0dc22a2ad0d16afc9b460a53f3fe
linelinlove <chrisline.lin@outlook.fr>
2023-01-24 01:04:31 +0100

    my team page, format mobile

---
+++ my-team-page/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            h1: ['36px', '36px'],
+            h2: ['18px', '18px'],
+            p : ['14px', '14px'],
+            caption : ['12px', '12px']
+        },
+        extend: {
+            fontFamily: {
+                'pt-serif': ['"PT Serif"'],
+                'poppins': ['"Poppins"'],
+                'montserrat': ['"Montserrat"']
+            },
+            colors: {
+                'light-grey': '#A9A9A9',
+              },
+        }
+    }
+}
