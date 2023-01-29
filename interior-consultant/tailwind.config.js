b4389765a8f80c11a3592bbc63df3393ba166331
linelinlove <chrisline.lin@outlook.fr>
2023-01-29 22:46:35 +0100

    interior consultant mobile display

---
+++ interior-consultant/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            h1: ['36px', '36px'],
+            h2: ['18px', '18px'],
+            p : ['14px', '14px'],
+            caption : ['12px', '12px']
+        },
+        extend: {
+            fontFamily: {
+                'crimson-pro': ['"Crimson Pro"'],
+                'lora': ['"Lora"'],
+                'montserrat': ['"Montserrat"']
+            },
+            colors: {
+                'light-grey': '#A9A9A9',
+                'light-grey2' : '#828282',
+                'light-black': '#181719'
+              },
+        }
+    }
+}
