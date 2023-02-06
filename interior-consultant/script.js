cf9bb7c2d576a8ffeab7314ad4aec1bdf1aa4693
linelinlove <chrisline.lin@outlook.fr>
2023-02-06 23:29:00 +0100

    mobile first

--- interior-consultant/script.js
+++ interior-consultant/script.js
@@ -1,6 +1,21 @@
-$(document).ready(function(){
-    $('ul li a').hover(function(){
+$(document).ready(function () {
+    
+    $('ul li a').hover(function () {
         $('li a').removeClass("currentpage");
         $(this).addClass("currentpage");
     });
+
+    const targetDiv = document.getElementById("show_menu");
+    const bodyoverflow = document.getElementById("overflow");
+    const btn = document.getElementById("openmenu");
+    btn.onclick = function () {
+        targetDiv.style.visibility = "visible";
+        bodyoverflow.style.overflow = "hidden";
+    };
+
+    const clsbtn = document.getElementById("closemenu");
+    clsbtn.onclick = function () {
+        targetDiv.style.visibility = "hidden";
+        bodyoverflow.style.overflow = "auto";
+    };
 });
