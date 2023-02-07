201b0982afaaeb9e6f12315fc914dee9db9d9d2e
linelinlove <chrisline.lin@outlook.fr>
2023-02-07 23:09:00 +0100

    rename dir and completed desktop display

--- 3-interior-consultant/tailwind.config.js
+++ 3-interior-consultant/tailwind.config.js
@@ -1,8 +1,10 @@
 tailwind.config = {
     theme: {
         fontSize: {
+            h0: ['48px', '48px'],
             h1: ['36px', '36px'],
             h2: ['18px', '18px'],
+            p2: ['24px', '24px'],
             p : ['14px', '14px'],
             caption : ['12px', '12px']
         },
