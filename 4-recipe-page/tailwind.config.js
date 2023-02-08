9be3420873d4dd6efda8f528a8ec6b2d9de994bd
linelinlove <chrisline.lin@outlook.fr>
2023-02-08 01:00:46 +0100

    recipe page

---
+++ 4-recipe-page/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            h0: ['48px', '48px'],
+            h1: ['36px', '36px'],
+            h2: ['18px', '18px'],
+            p2: ['24px', '24px'],
+            p : ['14px', '14px'],
+            caption : ['12px', '12px'],
+            m : ['10px', '10px'],
+            s : ['8px', '8px']
+        },
+        extend: {
+            fontFamily: {
+                'playfair-display': ['"Playfair Display"'],
+                'montserrat': ['"Montserrat"']
+            },
+            colors: {
+                'light-grey': '#4F4F4F',
+                'light-grey2' : '#BDBDBD',
+                'light-black': '#181719',
+                'p-orange': '#F2994A'
+              },
+        }
+    }
+}
