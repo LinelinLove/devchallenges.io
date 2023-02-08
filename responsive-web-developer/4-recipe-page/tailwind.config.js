d23a4af15117c0a81c93bf0bbf1c71b62d5cafec
linelinlove <chrisline.lin@outlook.fr>
2023-02-09 00:42:37 +0100

    rename dir and recipe page completed

--- responsive-web-developer/4-recipe-page/tailwind.config.js
+++ responsive-web-developer/4-recipe-page/tailwind.config.js
@@ -3,12 +3,13 @@ tailwind.config = {
         fontSize: {
             h0: ['48px', '48px'],
             h1: ['36px', '36px'],
-            h2: ['18px', '18px'],
             p2: ['24px', '24px'],
-            p : ['14px', '14px'],
-            caption : ['12px', '12px'],
-            m : ['10px', '10px'],
-            s : ['8px', '8px']
+            h2: ['18px', '18px'],
+            h1: ['16px', '16px'],
+            p: ['14px', '14px'],
+            caption: ['12px', '12px'],
+            m: ['10px', '10px'],
+            s: ['8px', '8px']
         },
         extend: {
             fontFamily: {
@@ -17,10 +18,13 @@ tailwind.config = {
             },
             colors: {
                 'light-grey': '#4F4F4F',
-                'light-grey2' : '#BDBDBD',
+                'light-grey2': '#BDBDBD',
                 'light-black': '#181719',
                 'p-orange': '#F2994A'
-              },
+            },
+            // margin: {
+            //     '350px': '350px',
+            // }
         }
     }
 }
