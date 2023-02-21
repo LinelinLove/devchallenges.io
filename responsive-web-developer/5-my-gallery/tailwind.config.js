23ad75a45235e20aed1957e5f37211b8f621d9df
linelinlove <chrisline.lin@outlook.fr>
2023-02-21 17:35:07 +0100

    fix and my gallery complete

---
+++ responsive-web-developer/5-my-gallery/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            h1: ['24px', '24px'],
+            p2: ['18px', '18px'],
+            p: ['14px', '14px'],
+            caption: ['12px', '12px'],
+            m: ['10px', '10px'],
+        },
+        extend: {
+            fontFamily: {
+                'montserrat': ['"Montserrat"']
+            },
+            colors: {
+                'light-grey': '#A9A9A9',
+                'light-grey2': '#BDBDBD',
+                'bg': '#E5E5E5',
+                'footer-grey': '#A9A9A9',
+            },
+        }
+    }
+}
