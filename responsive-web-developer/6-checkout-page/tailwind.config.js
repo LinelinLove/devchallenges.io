226fa5d8e1cd68859dc8fb74a437c9490d31dc65
linelinlove <chrisline.lin@outlook.fr>
2023-02-27 01:30:45 +0100

    complete checkout page and update readme

--- responsive-web-developer/6-checkout-page/tailwind.config.js
+++ responsive-web-developer/6-checkout-page/tailwind.config.js
@@ -1,9 +1,9 @@
 tailwind.config = {
     theme: {
         fontSize: {
-            h0: ['48px', '48px'],
-            h1: ['36px', '36px'],
+            h0: ['36px', '36px'],
             p2: ['24px', '24px'],
+            h3: ['22px', '22px'],
             h2: ['18px', '18px'],
             h1: ['16px', '16px'],
             p: ['14px', '14px'],
