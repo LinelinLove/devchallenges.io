50407f5760c22182dd2b075ab0a8e9c7f7496899
linelinlove <chrisline.lin@outlook.fr>
2023-02-28 01:36:39 +0100

    edie homepage mobile first

---
+++ responsive-web-developer/7-edie-homepage/README.md
@@ -0,0 +1,50 @@
+<h1 align="center">Edie Homepage</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://linelinlove.github.io/devchallenges.io/responsive-web-developer/7-edie-homepage/edie-homepage.html">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/xobQBuf8zWWmiYMIAZe0">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Contact](#contact)
+
+## Overview
+
+![screenshot](preview.png)
+
+- You can see the [live demo here](https://linelinlove.github.io/devchallenges.io/responsive-web-developer/7-edie-homepage/edie-homepage.html).
+
+### Built With
+
+- [HTML](https://www.w3schools.com/html/)
+- [Tailwind](https://tailwindcss.com/)
+- [JS](https://www.w3schools.com/js/)
+
+## Features
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/xobQBuf8zWWmiYMIAZe0) was to build an application to complete the given user stories.
+
+## Contact
+
+- Website [your-website.com](https://{your-web-site-link})
+- GitHub [@your-username](https://{github.com/your-usermame})
+- Twitter [@your-twitter](https://{twitter.com/your-username})
---
+++ responsive-web-developer/7-edie-homepage/tailwind.config.js
@@ -0,0 +1,55 @@
+tailwind.config = {
+    theme: {
+        fontSize: {
+            h1: ['36px', '36px'],
+            h2: ['24px', '24px'],
+            h3: ['22px', '22px'],
+            h4: ['18px', '18px'],
+            h5: ['16px', '16px'],
+            p: ['14px', '14px'],
+            caption: ['12px', '12px'],
+            m: ['10px', '10px'],
+        },
+        extend: {
+            fontFamily: {
+                'poppins': ['"Poppins"'],
+                'montserrat': ['"Montserrat"'],
+                'heebo': ['"Heebo"']
+            },
+            colors: {
+                'p-blue': '#2D9CDB',
+                'p-green': '#27AE60',
+                'p-red': '#EB5757',
+                'grey-white': '#F2F2F2',
+                'grey-white-dark': '#E0E0E0',
+                'grey-light': '#828282',
+                'grey-primary': '#4F4F4F',
+                'grey-dark': '#333333',
+                'footer-grey': '#BDBDBD',
+
+
+
+                // 'dark-grey2': '#4E5150',
+                // 'light-black': '#181719',
+                // 'p-orange0': '#FEFAF6',
+                // 'p-orange': '#F2994A',
+                // 'p-orange2': '#EF8424',
+                // 'p-orange3': '#DF7210',
+
+                // 'error': '#dd4040',
+                // 'checked': '#5ABB23',
+            },
+            animation: {
+                'shaking': 'shake 0.3s 1',
+            },
+            keyframes: {
+                shake: {
+                    '0%': { transform: 'translate(0px, 0px)' },
+                    '33%': { transform: 'translate(6px, 0px)' },
+                    '66%': { transform: 'translate(0px, 0px)' },
+                    '100%': { transform: 'translate(6px, 0px)' }
+                }
+            }
+        }
+    }
+}
