9896aa7108159d5c79be4280b571c1de7de3edaa
linelinlove <chrisline.lin@outlook.fr>
2023-03-09 00:02:00 +0100

    test deploy portfolio

--- responsive-web-developer/8-portfolio/vue.config.js
+++ responsive-web-developer/8-portfolio/vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: '/portfolio/'
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/devchallenges.io/responsive-web-developer/8-portfolio/dist/'
-  : '/'
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'devchallenges.io/responsive-web-developer/8-portfolio/dist/'
+    : '/'
+}
