5331ce1728df5a51d6f5313182bb11aa2fb74732
linelinlove <chrisline.lin@outlook.fr>
2023-03-08 23:23:23 +0100

    init portfolio

---
+++ responsive-web-developer/8-portfolio/src/components/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="h-[692px] w-[413px] border border-grey rounded-xl flex flex-col justify-start gap-y-8 m-4 p-6">
+    <img
+      src="..\assets\profile.jpg"
+      alt=""
+      class="w-[366px] border border-grey rounded-xl self-center"
+    />
+
+    <div>
+      <h1>Chrisline Lin</h1>
+      <h2>Front-end developer</h2>
+    </div>
+
+    <ul>
+      <li class="flex flex-row gap-x-5">
+        <span class="material-icons-round">mail</span>
+        <p>billy@example.com</p>
+      </li>
+      <li class="flex flex-row gap-x-5">
+        <span class="material-icons-round">phone</span>
+
+        <p>(+603) 546 624 342</p>
+      </li>
+    </ul>
+
+    <p>
+      Self-motivated developer, who is willing to learn and create outstanding
+      UI applications.
+    </p>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: "Profile",
+};
+</script>
+  
+<style scoped>
+</style>
+  
---
+++ responsive-web-developer/8-portfolio/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Profile></Profile>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Profile from '@/components/Profile.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Profile
+  }
+}
+</script>
