8ed41af7ae7f1f72a4dcaf86c33429287faced02
linelinlove <chrisline.lin@outlook.fr>
2023-05-07 17:35:38 +0200

    add tag filter

--- responsive-web-developer/8-portfolio/src/components/Project.vue
+++ responsive-web-developer/8-portfolio/src/components/Project.vue
@@ -8,76 +8,92 @@
           Projects ({{ data.projects.length }})
         </h1>
 
-        </div>
-
-        <div
-          v-for="(project, index) in data.projects.slice(0, visibleItemCount)"
-          :key="index"
-          class="flex flex-col items-start gap-y-9 mb-8 py-6 pl-4 pr-8 rounded-xl bg-white shadow-[0_0_30px_2px_rgba(0,0,0,0.1)]"
-        >
-          <div class="flex flex-row justify-between gap-x-8">
-            <img
-              :src="'./img/projects/' + project.image"
-              :alt="project.alt"
-              class="rounded-xl w-[322px] h-fit select-none active:border-p-blue-dark transform transition duration-200 hover:scale-105"
-            />
-
-            <div class="flex flex-col justify-between">
-              <div class="mb-2.5">
-                <div class="flex flex-row gap-x-3 mb-6 leading-5 gap-y-2.5 flex-wrap">
-                  <span
-                    v-for="tag in project.tag"
-                    :key="tag"
-                    class="transform transition duration-200 hover:scale-105 hover:font-semibold cursor-pointer"
-                    >#{{ tag }}</span
-                  >
-                </div>
-                <h1
-                  class="text-h2 leading-8 text-grey-primary font-semibold mb-5"
-                >
-                  {{ project.name }}
-                </h1>
-                <p>
-                  {{ project.description }}
-                </p>
-              </div>
-
-              <div class="flex flex-row gap-x-2.5 select-none">
-                <button
-                  class="py-2.5 px-10 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
-                >
-                  <a :href="project.demo">Demo</a>
-                </button>
-                <button
-                  class="py-2.5 px-10 bg-white text-p-blue-primary border border-p-blue-primary rounded-xl hover:bg-p-blue-light hover:text-white hover:border-p-blue-light active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
-                >
-                  <a :href="project.code">Code</a>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-row gap-x-10 justify-center">
+        <div class="flex flex-row gap-x-4">
           <button
-            @click="increaseLimit"
-            v-if="visibleItemCount < data.projects.length"
-            class="py-2.5 px-8 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
+            v-for="tag in getAllTags()"
+            :key="tag"
+            class="text-p text-grey-primary px-5 py-2 border rounded-xl border-grey-primary hover:text-p-blue hover:border-p-blue active:bg-p-blue active:text-white"
+            @click="toggleTag(tag)"
+            :class="{ active: selectedTag === tag }"
           >
-            View more
-          </button>
-          <button
-            @click="decreaseLimit"
-            v-if="visibleItemCount > minItem"
-            class="py-2.5 px-8 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
-          >
-            View less
+            {{ tag }}
           </button>
         </div>
       </div>
-      
-      <!-- data -->
-      
+
+      <div
+        v-for="(project, index) in filteredProjects.slice(0, visibleItemCount)"
+        :key="index"
+        class="flex flex-col items-start gap-y-9 mb-8 py-6 pl-4 pr-8 rounded-xl bg-white shadow-[0_0_30px_2px_rgba(0,0,0,0.1)]"
+      >
+        <div class="flex flex-row justify-between gap-x-8">
+          <img
+            :src="'./img/projects/' + project.image"
+            :alt="project.alt"
+            class="rounded-xl w-[322px] h-fit select-none active:border-p-blue-dark transform transition duration-200 hover:scale-105"
+          />
+
+          <div class="flex flex-col justify-between">
+            <div class="mb-2.5">
+              <div
+                class="flex flex-row gap-x-3 mb-6 leading-5 gap-y-2.5 flex-wrap"
+              >
+
+                <span
+                  v-for="tag in project.tag"
+                  :key="tag"
+                  class="transform transition duration-200 hover:scale-105 hover:font-semibold cursor-pointer"
+                  @click="toggleTag(tag)"
+                  :class="{ active: selectedTag === tag }"
+                >
+                  #{{ tag }}
+                </span>
+              </div>
+              <h1
+                class="text-h2 leading-8 text-grey-primary font-semibold mb-5"
+              >
+                {{ project.name }}
+              </h1>
+              <p>
+                {{ project.description }}
+              </p>
+            </div>
+
+            <div class="flex flex-row gap-x-2.5 select-none">
+              <button
+                class="py-2.5 px-10 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
+              >
+                <a :href="project.demo">Demo</a>
+              </button>
+              <button
+                class="py-2.5 px-10 bg-white text-p-blue-primary border border-p-blue-primary rounded-xl hover:bg-p-blue-light hover:text-white hover:border-p-blue-light active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
+              >
+                <a :href="project.code">Code</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row gap-x-10 justify-center">
+        <button
+          @click="increaseLimit"
+          v-if="visibleItemCount < data.projects.length"
+          class="py-2.5 px-8 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
+        >
+          View more
+        </button>
+        <button
+          @click="decreaseLimit"
+          v-if="visibleItemCount > minItem"
+          class="py-2.5 px-8 bg-p-blue-primary text-white border border-p-blue-primary rounded-xl hover:bg-p-blue hover:border-p-blue active:bg-p-blue-dark active:border-p-blue-dark transform transition duration-200 hover:scale-95"
+        >
+          View less
+        </button>
+      </div>
     </div>
+
+    <!-- data -->
+  </div>
 </template>
         
 
@@ -92,6 +108,8 @@ export default {
       visibleItemCount: 2,
       minItem: 2,
       showProjects: false,
+      selectedTag: null, // Tags sélectionnés
+      filteredProjects: [], // Liste des projets filtrés
     };
   },
 
@@ -101,6 +119,17 @@ export default {
     showMore() {
       return this.visibleItemCount < this.data.projects.length;
     },
+    filteredProjects() {
+      if (!this.selectedTag) {
+        // Si aucun tag n'est sélectionné, afficher tous les projets
+        return this.datas.data.projects;
+      } else {
+        // Filtrer les projets en fonction du tag sélectionné
+        return this.datas.data.projects.filter((project) => {
+          return project.tag.includes(this.selectedTag);
+        });
+      }
+    },
   },
   methods: {
     increaseLimit() {
@@ -108,6 +137,35 @@ export default {
     },
     decreaseLimit() {
       this.visibleItemCount -= 2;
+    },
+    filterProjects() {
+      if (this.selectedTags.length === 0) {
+        // Si aucun tag n'est sélectionné, afficher tous les projets
+        this.filteredProjects = this.data.projects;
+      } else {
+        // Filtrer les projets selon les tags sélectionnés
+        this.filteredProjects = this.data.projects.filter((project) =>
+          project.tag.some((tag) => this.selectedTags.includes(tag))
+        );
+      }
+    },
+    toggleTag(tag) {
+      if (this.selectedTag === tag) {
+        // Désélectionner le tag actuel si on clique à nouveau dessus
+        this.selectedTag = null;
+      } else {
+        // Sélectionner un nouveau tag
+        this.selectedTag = tag;
+      }
+    },
+    getAllTags() {
+      const tags = new Set(); // Utilisation d'un ensemble pour éviter les doublons
+      this.datas.data.projects.forEach((project) => {
+        project.tag.forEach((tag) => {
+          tags.add(tag);
+        });
+      });
+      return Array.from(tags); // Convertir l'ensemble en tableau
     },
   },
 };
